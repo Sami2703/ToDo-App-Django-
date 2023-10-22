3e9b702c54bfc801069b9fd95b7d8b284079ab2a
samikshas <samikshas@mkcl.org>
2023-10-22 15:13:29 +0530

    Todo App

---
+++ todo_main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from todo.models import Task
+
+def home(request):
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    # print(tasks)
+    completed_tasks = Task.objects.filter(is_completed= True)
+    # print(completed_tasks)
+    context={
+        'tasks': tasks,
+        'completed_tasks': completed_tasks,
+    }
+    return render(request, 'home.html', context)
